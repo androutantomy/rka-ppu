a9deb87313ded8400a5af927409d2ad898422583
Krisnawan Adi K <3275030504910027>
2023-02-26 17:03:53 +0700

    update

--- application/views/dashboard/belanja/cetak.php
+++ application/views/dashboard/belanja/cetak.php
@@ -587,7 +587,7 @@ menyesuaikan akun</div>
             <td class="column8 style42 null style43" colspan="6"></td>
           </tr>
           <tr class="row35">
-            <td class="column0 style38 s style40" colspan="8">Hj. Prof. Dr. Awang Faroek Ishak, M.M</td>
+            <td class="column0 style38 s style40" colspan="8">H. Prof. Dr. Awang Faroek Ishak, M.M</td>
             <td class="column8 style39 s style40" colspan="6">Hj. Dayang Donna Walfiaries Tania, S.Psi, M.M</td>
           </tr>
           <tr class="row36">
